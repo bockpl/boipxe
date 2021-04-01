86a43382f5840fd085f342be2b82704f0c2da16d
Wojciech Gabryjelski <wojciech.gabryjelski@p.lodz.pl>
2021-04-01 10:31:36 +0200

    Kasowanie /run/monit.pid

--- start.sh
+++ start.sh
@@ -9,4 +9,13 @@ MONIT_OPT=-I
 if ! [[ -z "$DEBUG" ]]; then
   MONIT_OPT="$MONIT_OPT -vvv"
 fi
+
+# kiedy monit startuje i znajduje wlasny pid /run/monit.pid,
+# to sprawdza czy proces o tym pid juz dziala
+# jezeli inny proces uruchomi sie na tym pid
+# to monit sie nie uruchamia
+if [[ -f /run/monit.pid ]]; then
+  rm /run/monit.pid
+fi
+
 monit $MONIT_OPT
